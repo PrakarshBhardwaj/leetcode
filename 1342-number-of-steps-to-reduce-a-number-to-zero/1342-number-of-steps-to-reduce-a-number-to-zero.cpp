51e34259dcf4d2fadcdbc587ca63fb3bd85d1647
Prakarsh Bhardwaj <56783108+PrakarshBhardwaj@users.noreply.github.com>
2022-05-27 10:16:51 +0530

    Time: 6 ms (6.14%), Space: 10.5 MB (26.66%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,15 +1,18 @@
-
+const int N = 1e6 + 1;
+static vector<int> V(N, -1);
 
 class Solution {
 public:
+    int go(int num){
+        if(num == 0) return 0;
+        if(V[num] != -1) return V[num];
+        
+        if(num&1) return V[num] = go(num - 1) + 1;
+        return V[num] = go(num/2) + 1;
+    }
+    
     int numberOfSteps(int num) {
         ios_base::sync_with_stdio(0);
-        int cnt = 0;
-        while(num) {
-            if(num & 1) num -= 1;
-            else num /= 2;
-            cnt++;
-        }
-        return cnt;
+        return go(num);
     }
 };
