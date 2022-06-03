83dbfc4f7a9a078435c0b1c5c12e39bef015fc93
Prakarsh Bhardwaj <56783108+PrakarshBhardwaj@users.noreply.github.com>
2022-06-03 21:54:12 +0530

    Time: 877 ms (12.17%), Space: 148.1 MB (48.25%) - LeetHub

---
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,51 @@
+class NumMatrix {
+public:
+    vector<vector<int>> pref;
+    
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        
+        pref = vector<vector<int>>(m, vector<int>(n));
+        
+        pref[0][0] = matrix[0][0];
+        
+        for(int i = 1; i < m; i++){
+            pref[i][0] = pref[i - 1][0] + matrix[i][0];
+        }
+        for(int i = 1; i < n; i++){
+            pref[0][i] = pref[0][i - 1] + matrix[0][i];
+        }
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                pref[i][j] = pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1] + matrix[i][j];
+            }
+        }
+        
+        // for(auto i : pref){
+        //     for(auto j : i){
+        //         cout << j << " ";
+        //     }
+        //     cout << endl;
+        // }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int tot = pref[row2][col2];
+        int to_sub = 0;
+        
+        if(row1 - 1 >= 0 && col1 - 1 >= 0) to_sub -= pref[row1 - 1][col1 - 1];
+        if(row1 - 1 >= 0) to_sub += pref[row1 - 1][col2];
+        if(col1 - 1 >= 0) to_sub += pref[row2][col1 - 1];
+        
+        // cout << tot << " " << to_sub;
+        
+        return tot - to_sub;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
