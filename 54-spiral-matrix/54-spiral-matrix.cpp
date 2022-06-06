b094e9a8294c40383c93d5c4b55a80c2cba8260a
Prakarsh Bhardwaj <56783108+PrakarshBhardwaj@users.noreply.github.com>
2022-06-06 23:15:58 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (31.41%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -5,7 +5,6 @@ public:
         int n = matrix[0].size();
         
         int ln = min(m, n)/2 + (min(m, n) & 1);
-        vector<pair<int, int>> v;
         set<pair<int, int>> s;
         vector<int> ans;
         
@@ -13,37 +12,31 @@ public:
             
             for(int j = i; j < n - i; j++){
                 if(s.find({i, j}) == s.end()){
-                    v.push_back({i, j});
+                    ans.push_back(matrix[i][j]);
                     s.insert({i,j});
                 }
             }
             
             for(int j = i; j < m - i; j++){
                 if(s.find({j, n - 1 - i}) == s.end()){
-                    v.push_back({j, n - 1 - i});  
+                    ans.push_back(matrix[j][n - 1 - i]);
                     s.insert({j, n - 1 - i});
                 }
             }
             
             for(int j = n - i - 1; j >= i; j--){
                 if(s.find({m - i - 1, j}) == s.end()){
-                    v.push_back({m - i - 1, j});
+                    ans.push_back(matrix[m - i - 1][j]);
                     s.insert({m - i - 1, j});
                 }
             }
             
             for(int j = m - 1 - i; j >= i; j--){
                 if(s.find({j, i}) == s.end()){
-                    v.push_back({j, i});
+                    ans.push_back(matrix[j][i]);
                     s.insert({j, i});
                 }
             }
-            
-            // v.pop_back();
-        }
-        
-        for(int i = 0; i < v.size(); i++){
-            ans.push_back(matrix[v[i].first][v[i].second]);
         }
         
         return ans;
