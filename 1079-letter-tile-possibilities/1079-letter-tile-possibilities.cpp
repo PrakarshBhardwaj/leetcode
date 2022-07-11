eb6a14f6900093c04e55f45db03fdd48e5dad100
Prakarsh Bhardwaj <56783108+PrakarshBhardwaj@users.noreply.github.com>
2022-07-12 00:51:20 +0530

    Time: 72 ms (60.58%), Space: 19.9 MB (14.35%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    unordered_set<int> ss;
-    const int inf = 1e9 + 7;
+    int ans = 0;
     
-    void go(string &tiles, int &n, int chash, int curr){
-        ss.insert(chash);
+    void go(string &tiles, int &n, int curr){
+        ans++;
+        unordered_set<char> s;
         
         for(int i = curr; i < n; i++){
-            swap(tiles[curr], tiles[i]);
-            go(tiles, n, ( (long long)33 * chash + (int)(tiles[curr] - 'A' + 1)) % inf, curr + 1);
-            swap(tiles[curr], tiles[i]);
+            if(s.find(tiles[i]) == s.end()){
+                s.insert(tiles[i]);
+                swap(tiles[curr], tiles[i]);
+                go(tiles, n, curr + 1);
+                swap(tiles[curr], tiles[i]);
+            }
         }
     }
     
     int numTilePossibilities(string tiles) {
         int n = tiles.size();
-        go(tiles, n, 0, 0);
-        return ss.size() - 1;
+        go(tiles, n, 0);
+        return ans - 1;
     }
 };
