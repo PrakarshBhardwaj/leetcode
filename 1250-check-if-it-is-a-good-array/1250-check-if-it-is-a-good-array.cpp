ee937ab1c9ff0258494bf827d6d4e36ff71b8016
Prakarsh Bhardwaj <56783108+PrakarshBhardwaj@users.noreply.github.com>
2022-07-17 02:15:13 +0530

    Time: 85 ms (36.50%), Space: 29.2 MB (68.84%) - LeetHub

--- 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -6,6 +6,7 @@ public:
     }
     
     bool isGoodArray(vector<int>& nums) {
+        // Linear Diophantine
         int n = nums.size();
         int tmp = 0;
         for(int i = 0; i < n; i++) tmp = gcd(tmp, nums[i]);
