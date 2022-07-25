ebd70cb47c446d0e303c6978127caa70380b8cd8
Prakarsh Bhardwaj <56783108+PrakarshBhardwaj@users.noreply.github.com>
2022-07-25 19:10:42 +0530

    Time: 17 ms (92.29%), Space: 12.8 MB (50.14%) - LeetHub

--- 514-freedom-trail/514-freedom-trail.cpp
+++ 514-freedom-trail/514-freedom-trail.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    int go(string &ring, string &key, int c, int i, vector<vector<int>> &dp, unordered_map<char, vector<int>> &mp){
+        int n1 = ring.length(), n2 = key.length();
+        if(c >= n2) return 0;
+        if(dp[i][c] != -1) return dp[i][c];
+        
+        int ans = INT_MAX;
+        
+        for(auto k : mp[key[c]]){
+            ans = min(ans, min(abs(k - i), n1 - abs(k - i)) + go(ring, key, c + 1, k, dp, mp) + 1);
+        }
+        
+        return dp[i][c] = ans;
+    }
+    
     int findRotateSteps(string ring, string key) {
         int n1 = ring.length(), n2 = key.length();
         unordered_map<char, vector<int>> mp;
@@ -8,26 +22,8 @@ public:
             mp[ring[i]].push_back(i);
         }
         
-        vector<vector<int>> dp(n1, vector<int>(n2));
+        vector<vector<int>> dp(n1, vector<int>(n2, -1));
         
-        for(int j = 0; j < n1; j++){
-            int tmp = INT_MAX;
-            for(auto k : mp[key[n2 - 1]]){
-                tmp = min(tmp, min(abs(k - j), n1 - abs(j - k)));
-            }
-            dp[j][n2 - 1] = tmp + 1;
-        }
-        
-        for(int i = n2 - 2; i >= 0; i--){
-            for(int j = 0; j < n1; j++){
-                int tmp = INT_MAX;
-                for(auto k : mp[key[i]]){
-                    tmp = min(tmp, min(abs(j - k), n1 - abs(j - k)) + dp[k][i + 1]);
-                }
-                dp[j][i] = tmp + 1;
-            }
-        }
-        
-        return dp[0][0];
+        return go(ring, key, 0, 0, dp, mp);
     }
 };
